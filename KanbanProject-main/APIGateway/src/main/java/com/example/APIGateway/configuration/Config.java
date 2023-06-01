fdd11d0ac28cba48cc93a14ddbfd6d085ea92dbd
priyanshunegi2398@gmail.com <priyanshunegi2398@gmail.com>
2023-06-01 11:13:13 +0530

    Initial commit

---
+++ KanbanProject-main/APIGateway/src/main/java/com/example/APIGateway/configuration/Config.java
@@ -0,0 +1,23 @@
+package com.example.APIGateway.configuration;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(p -> p.path("/api/v1/auth/**")
+                        .uri("http://authenticationservice:3033/"))
+                .route(p -> p.path("/api/v1/project/**")
+                        .uri("http://kanbanservice:8007/"))
+                .route(p -> p.path("/api/v1/user/**")
+                        .uri("http://kanbanservice:8007/"))
+                .route(p -> p.path("/api/v1/notifications/**")
+                        .uri("http://notificationservice:8090/"))
+                .build();
+    }
+}
